eda7acdd327dfd246a8505290d99b6d09689418a
Alejandro Fernandez-Sarasua <alfea1983@gmail.com>
2022-09-21 22:44:29 -0400

    changed bio text

--- src/routes/About.js
+++ src/routes/About.js
@@ -12,11 +12,11 @@ const About = () => {
         <div className="aboutText">
           <h1>Meet Alejandro</h1>
           <p>
-            Born in the Dominican Republic and raised in the USA, an Ironhack
-            graduate. I'm a programmer with a background in music and Audio
-            Visuals. I'm in love with The Arts and always looking for a creative
-            outlet. I am intertested in delivering top notch web applications
-            and sites.
+            Born in the Dominican Republic and raised in the USA. I'm a
+            programmer and Ironhack graduate with a background in music and
+            Audio Visuals. I'm in love with The Arts and always looking for a
+            creative outlet. I am intertested in delivering top notch web
+            applications and sites.
           </p>
           <br />
           <section id="techList">
