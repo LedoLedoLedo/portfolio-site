6edc1d66e3d93f15579564fba119c1934437d4a0
Alejandro Fernandez-Sarasua <alfea1983@gmail.com>
2022-09-24 21:41:17 -0400

    removed unnecessary div nesting

--- src/routes/About.js
+++ src/routes/About.js
@@ -3,35 +3,31 @@ import picMe from "../assets/alevegassmile.JPG";
 
 const About = () => {
   return (
-    <div>
-      <br />
-      <div className="aboutSection">
-        <div className="aboutPic">
-          <img classname="mePic" src={picMe} alt="me" />
-        </div>
-        <div className="aboutText">
-          <h1>Meet Alejandro</h1>
-          <p>
-            Born in the Dominican Republic and raised in the USA. I'm a
-            programmer and Ironhack graduate with a background in music and
-            Audio Visuals. I'm in love with The Arts and always looking for a
-            creative outlet. I am intertested in delivering top notch web
-            applications and sites.
-          </p>
-          <br />
-          <section id="techList">
-            <h3>Technologies</h3>
-            <ul>
-              <li>React</li>
-              <li>React Native</li>
-              <li>JavaScript</li>
-              <li>Express</li>
-              <li>Nodejs</li>
-              <li>HTML</li>
-              <li>CCS</li>
-            </ul>
-          </section>
-        </div>
+    <div className="aboutSection">
+      <div className="aboutPic">
+        <img classname="mePic" src={picMe} alt="me" />
+      </div>
+      <div className="aboutText">
+        <h1>Meet Alejandro</h1>
+        <p>
+          Born in the Dominican Republic and raised in the USA. I'm a programmer
+          and Ironhack graduate with a background in music and Audio Visuals.
+          I'm in love with The Arts and always looking for a creative outlet. I
+          am intertested in delivering top notch web applications and sites.
+        </p>
+        <br />
+        <section id="techList">
+          <h3>Technologies</h3>
+          <ul>
+            <li>React</li>
+            <li>React Native</li>
+            <li>JavaScript</li>
+            <li>Express</li>
+            <li>Nodejs</li>
+            <li>HTML</li>
+            <li>CCS</li>
+          </ul>
+        </section>
       </div>
     </div>
   );
