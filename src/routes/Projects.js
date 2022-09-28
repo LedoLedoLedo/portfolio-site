921b8b144a233d76f0ddedc89151ea77158fea75
Alejandro Fernandez-Sarasua <alfea1983@gmail.com>
2022-09-28 17:58:00 -0400

    added links to actual projects

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -4,7 +4,6 @@ import video1 from "../assets/weathermoodzrec.mov";
 import video2 from "../assets/gigatrecording720.mov";
 import video3 from "../assets/Breakingpoprec.mov";
 
-
 const projects = () => {
   return (
     <div id="ProjectsContainer">
@@ -15,7 +14,6 @@ const projects = () => {
        Created with - JavaScript, - React, - Express - CSS, - Mongodb compass and Postman, for testing. As well as The implementation of CRUD models to create a user experience and be able to dig down deeper in the local scenes."
         video={video2}
         path={"https://gigat.netlify.app/"}
-      
       />
 
       <SingleProject
@@ -23,7 +21,6 @@ const projects = () => {
         text="Created with the purpose of applying wellness and mental health awareness to our weather using - React and Weather APIs. Weather moods is a simple application to unite both. "
         video={video1}
         path={"https://weathermoods.surge.sh/"}
-      
       />
 
       <SingleProject
